52565e41eaab4cc7b737a46cbf0eff3cc34e5d8d
oncomouse <oncomouse@gmail.com>
2015-06-30 18:44:25 -0700

    documented

--- assets/javascripts/routes/generic.js
+++ assets/javascripts/routes/generic.js
@@ -1,9 +1,13 @@
 define([
+	// Libraries go here:
 ], function(
 
 ) {
 	var GenericRoute = function(app) {
 		app.clearAppCanvas();
+		
+		// Your code goes here:
+		
 		app.router.trigger('ready router:ready');
 	}
 	return GenericRoute;
