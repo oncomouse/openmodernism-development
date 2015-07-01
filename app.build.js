8c1f5b0165e5b711dba9040346627485d4c46298
oncomouse <oncomouse@gmail.com>
2015-06-30 17:52:16 -0700

    Now building compressed modules.

--- app.build.js
+++ app.build.js
@@ -16,10 +16,53 @@
 		'sprintf-vendor': '../vendor/sprintf/src/sprintf',
 		'writedown': 'vendor/writedown/writedown'
 	},
+	'shim': {
+		'lodash': {
+			'exports': '_'
+		},
+		'jquery-ui': {
+			'deps': ['jquery'],
+			'exports': '$'
+		},
+		'jst': {
+			'exports': 'JST'
+		},
+		'lorem': {
+			'exports': 'Lorem'
+		},
+		'sprintf': {
+			'exports': 'sprintf'
+		},
+		'bootstrap/affix':      { 'deps': ['jquery'], 'exports': '$.fn.affix' }, 
+		'bootstrap/alert':      { 'deps': ['jquery'], 'exports': '$.fn.alert' },
+		'bootstrap/button':     { 'deps': ['jquery'], 'exports': '$.fn.button' },
+		'bootstrap/carousel':   { 'deps': ['jquery'], 'exports': '$.fn.carousel' },
+		'bootstrap/collapse':   { 'deps': ['jquery'], 'exports': '$.fn.collapse' },
+		'bootstrap/dropdown':   { 'deps': ['jquery'], 'exports': '$.fn.dropdown' },
+		'bootstrap/modal':      { 'deps': ['jquery'], 'exports': '$.fn.modal' },
+		'bootstrap/popover':    { 'deps': ['jquery'], 'exports': '$.fn.popover' },
+		'bootstrap/scrollspy':  { 'deps': ['jquery'], 'exports': '$.fn.scrollspy' },
+		'bootstrap/tab':        { 'deps': ['jquery'], 'exports': '$.fn.tab'        },
+		'bootstrap/tooltip':    { 'deps': ['jquery'], 'exports': '$.fn.tooltip' },
+		'bootstrap/transition': { 'deps': ['jquery'], 'exports': '$.fn.transition' }
+	},
 	'map': {
 		'*': {
 			'underscore': 'lodash'
 		}
 	},
+	'modules': [
+		{
+			'name': 'main'
+		},
+		{
+			'name': 'routes/document',
+			'exclude': ['main']
+		},
+		{
+			'name': 'routes/documents',
+			'exclude': ['main']
+		}
+	],
 	'skipDirOptimize': false
 })
