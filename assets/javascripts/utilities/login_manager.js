4d7f898cc099a69e8b3a6cf64d5063b1f7a28d3c
oncomouse <oncomouse@gmail.com>
2015-07-19 10:45:37 -0400

    login now totally handled w/ react components

--- assets/javascripts/utilities/login_manager.js
+++ assets/javascripts/utilities/login_manager.js
@@ -4,34 +4,28 @@ define([
 	'views/login/login_view',
 	//'views/login/login_link_view',
 	'components/login/login_link',
+	'components/login/login_modal',
 	'utilities/alert_manager',
 	'postal'
 ], function(
 	LoginView,
 	LoginLinkComponent,
+	LoginModalComponent,
 	AlertManager,
 	postal
 ){
 	var LoginManager = Backbone.Model.extend({
-		initialize: function() {
-			this.login_view = new LoginView({model: this});
-			//this.login_link_view = new LoginLinkView({model: this});
-			
-			this.login_view.render();
-			//this.login_link_view.render();
-			
+		initialize: function() {			
 			this.login_link = new LoginLinkComponent;
+			this.login_modal = new LoginModalComponent;
 			
 			this.channel = {};
 			this.channel['login'] = postal.channel('login');
 			this.channel['component'] = postal.channel('component');
 			
 			this.channel['login'].subscribe('submit', _.bind(function(data, envelope) {
-				this.form_submit(data.event);
+				this.form_submit(data.formType, data.event);
 			}, this));
-			this.channel['login'].subscribe('show-modal', function(data, envelope) {
-				$('#LoginModal').modal('show');
-			});
 			this.channel['component'].subscribe('register', _.bind(function(data, envelope) {
 				if(data.component == 'LoginLink') {
 					this.authenticate();
@@ -47,100 +41,23 @@ define([
 		authentication_status: function() {
 			return false;
 		},
-		form_submit: function(ev) {
-			var class_name;
-			
-			class_name = $('#LoginModal form').attr('action').replace(/^\#/,'');
-			
-			switch(class_name) {
-				case 'login-form':
+		form_submit: function(form_type, ev) {
+			switch(form_type) {
+				case 'login':
 					this.form_submit_login();
 					break;
-				case 'create-form':
+				case 'create_user':
 					this.form_submit_create();
 					break;
 			}
-			return false;
 		},
-		form_submit_login: function() {
-			var form_values, $email_input, $password_input;
-			var validation = true;
-			
-			$email_input = $('#LoginModal form').find('#email');
-			$password_input = $('#LoginModal form').find('#password');
-			
-			if($email_input.get(0).validity.valid) {
-				AlertManager.clear_alert('email-alert');
-				$email_input.parent().removeClass('has-error')
-			} else {
-				if(!$email_input.parent().hasClass('has-error')) {
-					AlertManager.show_alert({
-						type: 'danger',
-						message: 'Invalid Email Address',
-						timeout: null,
-						target: $email_input.parent().find('label'),
-						dismissable: false,
-						id: 'email-alert'
-					});
-					$email_input.parent().addClass('has-error');
-				}
-				validation = false;
-			}
-			
-			if(validation) {
-				form_values = {
-					email: $email_input.val(),
-					password: $password_input.val()
-				};
-				
-				this.channel['login'].publish('submitted');
-			}
+		form_submit_login: function(ev) {
+			// Some kind of AJAX call:
+			this.channel['login'].publish('submitted');
 		},
-		form_submit_create: function() {
-			var form_values, $email_input, $password_input, $password2_input, $first_input;
-			var validation = true;
-			
-			$email_input = $('#LoginModal form').find('#email');
-			$password_input = $('#LoginModal form').find('#password');
-			$password2_input = $('#LoginModal form').find('#password2');
-			$first_input = $('#LoginModal form input').eq(0);
-			
-			if($email_input.get(0).validity.valid) {
-				AlertManager.clear_alert('email-alert');
-				$email_input.parent().toggleClass('has-error');
-			} else {
-				if(!$email_input.parent().hasClass('has-error')) {
-					AlertManager.show_alert({
-						type: 'danger',
-						message: 'Invalid Email Address',
-						timeout: null,
-						target: $first_input.parent().find('label'),
-						dismissable: false,
-						id: 'email-alert'
-					});
-					$email_input.parent().toggleClass('has-error');
-				}
-				validation = false;
-			}
-			if($password_input.val() == $password2_input.val()) {
-				AlertManager.clear_alert('password-alert');
-				$password_input.parent().toggleClass('has-error');
-				$password2_input.parent().toggleClass('has-error');
-			} else {
-				if(!$password_input.parent().hasClass('has-error')) {
-					AlertManager.show_alert({
-						type: 'danger',
-						message: 'Passwords Must Match',
-						timeout: null,
-						target: $first_input.parent().find('label'),
-						dismissable: false,
-						id: 'password-alert'
-					})
-					$password_input.parent().toggleClass('has-error');
-					$password2_input.parent().toggleClass('has-error');
-				}
-				validation = false;
-			}
+		form_submit_create: function(ev) {
+			// Some kind of AJAX call:
+			this.channel['login'].publish('submitted');
 		},
 		authenticated: function() {
 			return false;
